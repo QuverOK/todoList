771e2eb1d848c42a3cdb5bf7f99a43894d032241
Danila <112901749+QuverOK@users.noreply.github.com>
2023-10-12 19:57:49 +0300

    Delete src directory

--- src/components/TodoList.jsx
+++
@@ -1,9 +0,0 @@
-export const TodoList = ({ handleClick }) => {
-  return (
-    <ul className="todo__list">
-      {todoList.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} onDelete={handleClick} />
-      ))}
-    </ul>
-  )
-}
